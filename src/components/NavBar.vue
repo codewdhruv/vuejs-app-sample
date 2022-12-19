a18f93bb8e0d4c2ed66c72107541ea248fd82a1e
codewdhruv <dhruba.chakraborty2019@gift.edu.in>
2022-12-20 05:15:25 +0530

    first commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div
+      class="collapse navbar-collapse justify-content-md-center"
+      id="navbarsExample08"
+    >
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">
+            Harness Community Vue App Sample <span class="sr-only">(current)</span></a
+          >
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
